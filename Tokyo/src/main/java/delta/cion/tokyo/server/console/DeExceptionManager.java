ea0d536befe5ef0c832f2975957e4a5c669e8e82
Nionim <delta.cion.fe@gmail.com>
2026-06-29 20:42:23 +0300

    Try to hide network errors from console x2

--- Tokyo/src/main/java/delta/cion/tokyo/server/console/DeExceptionManager.java
+++ Tokyo/src/main/java/delta/cion/tokyo/server/console/DeExceptionManager.java
@@ -5,13 +5,13 @@ import net.minestom.server.MinecraftServer;
 public class DeExceptionManager {
 	public static void init() {
 		MinecraftServer.getExceptionManager().setExceptionHandler(e -> {
-			if (e instanceof RuntimeException runtimeException) {
-				Throwable cause = runtimeException.getCause();
-				if (cause instanceof IllegalArgumentException &&
-					cause.getMessage() != null &&
-					cause.getMessage().contains("String is too long")) {
-					return;
-				}
+			if (e instanceof RuntimeException re && re.getCause() instanceof IllegalArgumentException iae
+				&& "String is too long".equals(iae.getMessage())) {
+				return;
+			}
+			if (e instanceof IllegalStateException ise && ise.getMessage() != null
+				&& ise.getMessage().contains("Packet id") && ise.getMessage().contains("isn't registered!")) {
+				return;
 			}
 			e.printStackTrace();
 		});
